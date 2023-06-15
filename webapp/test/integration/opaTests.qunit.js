8e72a4c8968d17ec195fa101d61a48c01bb991b6
Muraleedhar K <muralikulkarni.mk@gmail.com>
2023-06-15 08:02:12 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/crossapp/crossapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comcrossapp/crossapp/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
